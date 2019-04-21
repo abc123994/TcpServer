ca2dc0f69af793b6a0e11d8838bc5f42ad4f42ca
abc123994 <m263501823@gmail.com>
2019-04-21 17:54:20 +0800

    Modify Protocol

--- Source/Schema/Protocol.h
+++ Source/Schema/Protocol.h
@@ -2,7 +2,30 @@
 //和客端的協議
 struct MSG_rep {
 	QString message;
+	QString content;
 	QJsonObject toJson() const {
-		return {  {"msg", message} };
+	
+		return { {"msg", message},{"content",content} };
 	}
+};
+
+struct TypeA
+{
+	int A_number;
+
+	QJsonObject toJson() const {
+		
+		return { {"A_number", A_number} };
+	}
+
+};
+
+struct TypeB
+{
+	QString B_String;
+	QJsonObject toJson() const {
+
+		return { {"B_String", B_String} };
+	}
+
 };
